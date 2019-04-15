bf25d01572a58e1b09089a60906945a911240bd9
iFayazAhmed21 <49371307+iFayazAhmed21@users.noreply.github.com>
2019-04-15 22:49:51 +0500

    Add files via upload

---
+++ 18SW21 lab2/prog3.java
@@ -0,0 +1,7 @@
+class Prog3
+{
+public static void main(String[]args){
+ System.out.println("Name:"+args[0]);
+ System.out.println("Rollno:"+args[1]);
+}
+}
---
+++ 18SW21 lab2/prog7.java
@@ -0,0 +1,14 @@
+class Prog7{
+public static void main(String[]args){
+ int a=6;
+ int b=7;
+ int c=a&b;
+
+System.out.println(c);
+}
+}
+
+
+
+
+
